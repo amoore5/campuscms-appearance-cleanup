e90d429e98e6f6d100024edf106c1be182d9a260
Adam Moore <amoore5@ucmerced.edu>
2012-02-01 12:20:36 -0800

    Fix typo Remove disable link

--- campuscms_appearance_cleanup.js
+++ campuscms_appearance_cleanup.js
@@ -4,6 +4,7 @@
       $("a[title='Settings for Seven theme']").closest('.theme-selector').hide();
       $(".system-themes-list-disabled").hide();
       $("#system-themes-admin-form").hide();
+      $('.operations .1').hide();
     }
   };
 })(jQuery);
